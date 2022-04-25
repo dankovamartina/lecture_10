3ebc6d62e3fbd41de2b7c953ebf39b84ea2fedb2
Martina Dankova <andorianka@gmail.com>
2022-04-25 08:51:25 +0200

    Implemented function for creatign a dictionary

--- sorting.py
+++ sorting.py
@@ -1,5 +1,5 @@
 import os
-
+import csv
 
 def read_data(file_name):
     """
@@ -10,10 +10,30 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, mode = "r") as csv_file:
+        reader = csv.DictReader(csv_file) #funkce vytvari slovniky, z kazdeho radku vyrobi slovnik, klicem je nazev sloupce  ahodnotou polozka v danem sloupci a radku
+        for row in reader:
+            print(row)
+# ted jsme vytvorili nekolik slovniku a ted je spojime do jednoto slovniku velkeho
+    with open(file_path, mode = "r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        big_dictionary = {}
+        for row in reader:
+            for key, value in row.items():
+                if key not in big_dictionary.keys():
+                    big_dictionary[key] = [int(value)] #musime to dat do slovniku, protoze pak chceme appendovat
+                else:
+                    big_dictionary[key].append(int(value))
+        return big_dictionary
+        #ziskali jsme jeden slovnik, kde klice jsou nazvy sloupcu a hodnoty seznam cisel
+
+
+
+
 
 
 def main():
-    pass
+    print(read_data("numbers.csv"))
 
 
 if __name__ == '__main__':
